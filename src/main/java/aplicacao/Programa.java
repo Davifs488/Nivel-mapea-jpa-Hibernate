8a4719f6238afc20814986ee225d0906efda9d31
Davi Ferreira da Silva <sildv45@gmail.com>
2023-04-11 23:29:47 -0300

    Incluindo JPA para persistir os objetos em banco de dados

---
+++ src/main/java/aplicacao/Programa.java
@@ -0,0 +1,26 @@
+package aplicacao;
+
+import dominio.Pessoa;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		
+		Pessoa p1 = new Pessoa(1,"Sara Sarada lima", "sallimma@gmail.com");
+		Pessoa p2 = new Pessoa(1,"Ramiro M. lima", "rmiraa@gmail.com");
+		Pessoa p3 = new Pessoa(1,"Silvio Felipo Amaral", "silipoma@gmail.com");
+		Pessoa p4 = new Pessoa(1,"Judas Tavares ", "tajuda@gmail.com");
+		Pessoa p5 = new Pessoa(1,"ggggggggggggggglllll", "silipoma@gmail.com");
+		Pessoa p6 = new Pessoa(1,"hxjcnvuvnvnnv ", "tajuda@gmail.com");
+		System.out.println(p1);
+		System.out.println(p2);
+		System.out.println(p3);
+		System.out.println(p4);
+		System.out.println(p5);
+		System.out.println(p6);
+
+
+	}
+
+}
